411088b738c0a577709c599e1bd74d26dd720bfa
Patyi Szabolcs <patyi.szabolcs@diak.szbi-pg.hu>
2025-11-10 09:47:29 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace PSZ_Dobokocka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pctb_aladar = new System.Windows.Forms.PictureBox();
+            this.pctb_bendeguz = new System.Windows.Forms.PictureBox();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.lbl_aladar = new System.Windows.Forms.Label();
+            this.lbl_bendeguz = new System.Windows.Forms.Label();
+            this.lbl_aladar_dobasok = new System.Windows.Forms.Label();
+            this.lbl_bendeguz_dobasok = new System.Windows.Forms.Label();
+            this.lbl_aladar_gyozelmek = new System.Windows.Forms.Label();
+            this.lbl_bendeguz_gyozelmek = new System.Windows.Forms.Label();
+            this.lbl_aladar_6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_bendeguz_6 = new System.Windows.Forms.Label();
+            this.lbl_aladar_szeria = new System.Windows.Forms.Label();
+            this.lbl_bendeguz_szeria = new System.Windows.Forms.Label();
+            this.idozito = new System.Windows.Forms.Timer(this.components);
+            this.btn_indulas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctb_aladar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctb_bendeguz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctb_aladar
+            // 
+            this.pctb_aladar.Location = new System.Drawing.Point(59, 258);
+            this.pctb_aladar.Name = "pctb_aladar";
+            this.pctb_aladar.Size = new System.Drawing.Size(152, 180);
+            this.pctb_aladar.TabIndex = 0;
+            this.pctb_aladar.TabStop = false;
+            // 
+            // pctb_bendeguz
+            // 
+            this.pctb_bendeguz.Location = new System.Drawing.Point(552, 258);
+            this.pctb_bendeguz.Name = "pctb_bendeguz";
+            this.pctb_bendeguz.Size = new System.Drawing.Size(152, 180);
+            this.pctb_bendeguz.TabIndex = 0;
+            this.pctb_bendeguz.TabStop = false;
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_title.Location = new System.Drawing.Point(306, 9);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(185, 25);
+            this.lbl_title.TabIndex = 1;
+            this.lbl_title.Text = "Dobókocka verseny";
+            this.lbl_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_aladar
+            // 
+            this.lbl_aladar.AutoSize = true;
+            this.lbl_aladar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_aladar.Location = new System.Drawing.Point(55, 56);
+            this.lbl_aladar.Name = "lbl_aladar";
+            this.lbl_aladar.Size = new System.Drawing.Size(59, 20);
+            this.lbl_aladar.TabIndex = 2;
+            this.lbl_aladar.Text = "Aladár:";
+            // 
+            // lbl_bendeguz
+            // 
+            this.lbl_bendeguz.AutoSize = true;
+            this.lbl_bendeguz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_bendeguz.Location = new System.Drawing.Point(547, 56);
+            this.lbl_bendeguz.Name = "lbl_bendeguz";
+            this.lbl_bendeguz.Size = new System.Drawing.Size(86, 20);
+            this.lbl_bendeguz.TabIndex = 2;
+            this.lbl_bendeguz.Text = "Bendegúz:";
+            // 
+            // lbl_aladar_dobasok
+            // 
+            this.lbl_aladar_dobasok.AutoSize = true;
+            this.lbl_aladar_dobasok.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_aladar_dobasok.Location = new System.Drawing.Point(56, 95);
+            this.lbl_aladar_dobasok.Name = "lbl_aladar_dobasok";
+            this.lbl_aladar_dobasok.Size = new System.Drawing.Size(72, 17);
+            this.lbl_aladar_dobasok.TabIndex = 3;
+            this.lbl_aladar_dobasok.Text = "Dobások: ";
+            // 
+            // lbl_bendeguz_dobasok
+            // 
+            this.lbl_bendeguz_dobasok.AutoSize = true;
+            this.lbl_bendeguz_dobasok.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_bendeguz_dobasok.Location = new System.Drawing.Point(548, 95);
+            this.lbl_bendeguz_dobasok.Name = "lbl_bendeguz_dobasok";
+            this.lbl_bendeguz_dobasok.Size = new System.Drawing.Size(72, 17);
+            this.lbl_bendeguz_dobasok.TabIndex = 3;
+            this.lbl_bendeguz_dobasok.Text = "Dobások: ";
+            // 
+            // lbl_aladar_gyozelmek
+            // 
+            this.lbl_aladar_gyozelmek.AutoSize = true;
+            this.lbl_aladar_gyozelmek.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_aladar_gyozelmek.Location = new System.Drawing.Point(56, 137);
+            this.lbl_aladar_gyozelmek.Name = "lbl_aladar_gyozelmek";
+            this.lbl_aladar_gyozelmek.Size = new System.Drawing.Size(86, 17);
+            this.lbl_aladar_gyozelmek.TabIndex = 4;
+            this.lbl_aladar_gyozelmek.Text = "Győzelmek: ";
+            // 
+            // lbl_bendeguz_gyozelmek
+            // 
+            this.lbl_bendeguz_gyozelmek.AutoSize = true;
+            this.lbl_bendeguz_gyozelmek.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_bendeguz_gyozelmek.Location = new System.Drawing.Point(548, 137);
+            this.lbl_bendeguz_gyozelmek.Name = "lbl_bendeguz_gyozelmek";
+            this.lbl_bendeguz_gyozelmek.Size = new System.Drawing.Size(86, 17);
+            this.lbl_bendeguz_gyozelmek.TabIndex = 4;
+            this.lbl_bendeguz_gyozelmek.Text = "Győzelmek: ";
+            // 
+            // lbl_aladar_6
+            // 
+            this.lbl_aladar_6.AutoSize = true;
+            this.lbl_aladar_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_aladar_6.Location = new System.Drawing.Point(56, 179);
+            this.lbl_aladar_6.Name = "lbl_aladar_6";
+            this.lbl_aladar_6.Size = new System.Drawing.Size(98, 17);
+            this.lbl_aladar_6.TabIndex = 5;
+            this.lbl_aladar_6.Text = "6-os dobások:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "6-os dobások:";
+            // 
+            // lbl_bendeguz_6
+            // 
+            this.lbl_bendeguz_6.AutoSize = true;
+            this.lbl_bendeguz_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_bendeguz_6.Location = new System.Drawing.Point(548, 179);
+            this.lbl_bendeguz_6.Name = "lbl_bendeguz_6";
+            this.lbl_bendeguz_6.Size = new System.Drawing.Size(98, 17);
+            this.lbl_bendeguz_6.TabIndex = 5;
+            this.lbl_bendeguz_6.Text = "6-os dobások:";
+            // 
+            // lbl_aladar_szeria
+            // 
+            this.lbl_aladar_szeria.AutoSize = true;
+            this.lbl_aladar_szeria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_aladar_szeria.Location = new System.Drawing.Point(59, 216);
+            this.lbl_aladar_szeria.Name = "lbl_aladar_szeria";
+            this.lbl_aladar_szeria.Size = new System.Drawing.Size(96, 17);
+            this.lbl_aladar_szeria.TabIndex = 6;
+            this.lbl_aladar_szeria.Text = "Nyerő széria: ";
+            // 
+            // lbl_bendeguz_szeria
+            // 
+            this.lbl_bendeguz_szeria.AutoSize = true;
+            this.lbl_bendeguz_szeria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_bendeguz_szeria.Location = new System.Drawing.Point(548, 216);
+            this.lbl_bendeguz_szeria.Name = "lbl_bendeguz_szeria";
+            this.lbl_bendeguz_szeria.Size = new System.Drawing.Size(96, 17);
+            this.lbl_bendeguz_szeria.TabIndex = 6;
+            this.lbl_bendeguz_szeria.Text = "Nyerő széria: ";
+            // 
+            // idozito
+            // 
+            this.idozito.Tick += new System.EventHandler(this.idozito_Tick);
+            // 
+            // btn_indulas
+            // 
+            this.btn_indulas.BackColor = System.Drawing.Color.SkyBlue;
+            this.btn_indulas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_indulas.Location = new System.Drawing.Point(359, 44);
+            this.btn_indulas.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_indulas.Name = "btn_indulas";
+            this.btn_indulas.Size = new System.Drawing.Size(58, 32);
+            this.btn_indulas.TabIndex = 7;
+            this.btn_indulas.Text = "Indulj!";
+            this.btn_indulas.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_indulas.UseVisualStyleBackColor = false;
+            this.btn_indulas.Click += new System.EventHandler(this.btn_indulas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_indulas);
+            this.Controls.Add(this.lbl_bendeguz_szeria);
+            this.Controls.Add(this.lbl_aladar_szeria);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_bendeguz_6);
+            this.Controls.Add(this.lbl_aladar_6);
+            this.Controls.Add(this.lbl_bendeguz_gyozelmek);
+            this.Controls.Add(this.lbl_aladar_gyozelmek);
+            this.Controls.Add(this.lbl_bendeguz_dobasok);
+            this.Controls.Add(this.lbl_aladar_dobasok);
+            this.Controls.Add(this.lbl_bendeguz);
+            this.Controls.Add(this.lbl_aladar);
+            this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.pctb_bendeguz);
+            this.Controls.Add(this.pctb_aladar);
+            this.Name = "Form1";
+            this.Text = "Dobókocka verseny";
+            ((System.ComponentModel.ISupportInitialize)(this.pctb_aladar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctb_bendeguz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctb_aladar;
+        private System.Windows.Forms.PictureBox pctb_bendeguz;
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Label lbl_aladar;
+        private System.Windows.Forms.Label lbl_bendeguz;
+        private System.Windows.Forms.Label lbl_aladar_dobasok;
+        private System.Windows.Forms.Label lbl_bendeguz_dobasok;
+        private System.Windows.Forms.Label lbl_aladar_gyozelmek;
+        private System.Windows.Forms.Label lbl_bendeguz_gyozelmek;
+        private System.Windows.Forms.Label lbl_aladar_6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_bendeguz_6;
+        private System.Windows.Forms.Label lbl_aladar_szeria;
+        private System.Windows.Forms.Label lbl_bendeguz_szeria;
+        private System.Windows.Forms.Timer idozito;
+        private System.Windows.Forms.Button btn_indulas;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSZ_Dobokocka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static Random rnd = new Random();
+        static int kep_index_aladar;
+        static int kep_index_bendeguz;
+        public List<int> aladar_list;
+        public List<int> bendeguz_list;
+
+        private void idozito_Tick(object sender, EventArgs e)
+        {
+            kep_index_aladar = rnd.Next(1, 7);
+            kep_index_bendeguz = rnd.Next(1, 7);
+            switch (kep_index_aladar)
+            {
+                case 1:
+                    pctb_aladar.Load("dobas1.png");
+                    break;
+                case 2:
+                    pctb_aladar.Load("dobas2.png");
+                    break;
+                case 3:
+                    pctb_aladar.Load("dobas3.png");
+                    break;
+                case 4:
+                    pctb_aladar.Load("dobas4.png");
+                    break;
+                case 5:
+                    pctb_aladar.Load("dobas5.png");
+                    break;
+                case 6:
+                    pctb_aladar.Load("dobas6.png");
+                    break;
+
+
+            }
+
+            switch (kep_index_bendeguz)
+            {
+                case 1:
+                    pctb_bendeguz.Load("dobas1.png");
+                    break;
+                case 2:
+                    pctb_bendeguz.Load("dobas2.png");
+                    break;
+                case 3:
+                    pctb_bendeguz.Load("dobas3.png");
+                    break;
+                case 4:
+                    pctb_bendeguz.Load("dobas4.png");
+                    break;
+                case 5:
+                    pctb_bendeguz.Load("dobas5.png");
+                    break;
+                case 6:
+                    pctb_bendeguz.Load("dobas6.png");
+                    break;
+            }
+        }
+
+        private void btn_indulas_Click(object sender, EventArgs e)
+        {
+            idozito.Enabled = true;
+            btn_indulas.Text = "Állj";
+        }
+    }
+}
